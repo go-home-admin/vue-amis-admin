c5b74ea80827f7ed218e5d0e58f018902bd72274
yuanzhao <2206582181@qq.com>
2023-02-03 10:15:23 +0800

    init

---
+++ src/views/documentation/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="box" />
+</template>
+
+<script>
+
+export default {
+  name: 'Amis',
+  data() {
+    return {}
+  },
+  mounted() {
+    // eslint-disable-next-line no-undef
+    var amis = amisRequire('amis/embed')
+    console.log(amis)
+
+    amis.embed('#box', {
+      type: 'page',
+      title: '表单页面',
+      body: {
+        type: 'form',
+        mode: 'horizontal',
+        api: '/saveForm',
+        body: [
+          {
+            label: 'Name',
+            type: 'input-text',
+            name: 'name'
+          },
+          {
+            label: 'Email',
+            type: 'input-email',
+            name: 'email'
+          }
+        ]
+      }
+    })
+  }
+}
+</script>
