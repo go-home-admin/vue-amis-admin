1611aaa14d44451cfd0fcd28d4be5ccd73d47166
yuanzhao <2206582181@qq.com>
2023-03-23 14:31:44 +0800

    amis路由解决

--- src/router/app.js
+++ src/router/app.js
@@ -1,10 +1,23 @@
+import Layout from '@/layout'
+
 // 遍历 views 文件夹下的全部 vue 文件
 const requireViews = require.context('@/views', true, /\w+\.vue$/)
 
-const AppRouter = []
+const AppRouter = [
+  {
+    path: 'Layout',
+    name: 'Layout',
+    component: Layout
+  }
+]
+// 过滤目录
+const excludedPrefixes = ['dashboard/admin/', 'error-', 'login/', 'profile/components/']
 
 requireViews.keys().map((viewPath, index) => {
   const viewName = viewPath.split('.vue')[0].substr(2)
+
+  const excludedPrefix = excludedPrefixes.find(prefix => viewPath.startsWith(`./${prefix}`))
+  if (excludedPrefix) return
 
   AppRouter[viewName] = {
     path: viewName === 'index' ? '/' : `/${viewName}`,
--- src/router/index.js
+++ src/router/index.js
@@ -99,48 +99,6 @@ export const constantRoutes = [
  */
 export const asyncRoutes = [
   {
-    path: '/permission',
-    component: Layout,
-    redirect: '/permission/page',
-    alwaysShow: true, // will always show the root menu
-    name: 'Permission',
-    meta: {
-      title: 'permission',
-      icon: 'lock',
-      roles: ['admin', 'editor'] // you can set roles in root nav
-    },
-    children: [
-      {
-        path: 'page',
-        component: () => import('@/views/permission/page'),
-        name: 'PagePermission',
-        meta: {
-          title: 'pagePermission',
-          roles: ['admin'] // or you can only set roles in sub nav
-        }
-      },
-      {
-        path: 'directive',
-        component: () => import('@/views/permission/directive'),
-        name: 'DirectivePermission',
-        meta: {
-          title: 'directivePermission'
-          // if do not set roles, means: this page does not require permission
-        }
-      },
-      {
-        path: 'role',
-        component: () => import('@/views/permission/role'),
-        name: 'RolePermission',
-        meta: {
-          title: 'rolePermission',
-          roles: ['admin']
-        }
-      }
-    ]
-  },
-
-  {
     path: '/error',
     component: Layout,
     redirect: 'noRedirect',
@@ -187,19 +145,6 @@ export const asyncRoutes = [
         component: () => import('@/views/theme/index'),
         name: 'Theme',
         meta: { title: 'theme', icon: 'theme' }
-      }
-    ]
-  },
-
-  {
-    path: '/clipboard',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/clipboard/index'),
-        name: 'ClipboardDemo',
-        meta: { title: 'clipboardDemo', icon: 'clipboard' }
       }
     ]
   },
